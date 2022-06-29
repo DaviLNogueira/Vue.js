11fb3e6bc91b9720672c14946574e602e270f582
Davi Lima Nogueira <davi_nogueira@ufms.br>
2022-06-29 10:07:09 -0400

    Importação de VueResource e implementação de array de fotos

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <h1>{{titulo}}</h1>
+  <div>
+    <h1>{{titulo}}</h1>
+
+  <ul>
+    <!--v-for realiza a um loop da variavell no script-->
+    <li v-for="foto of fotos"><img :src="foto.url" :alt="foto.descricao"></li>
+
+  </ul>
+  </div>
+
+
 </template>
 
 <script>
 export default {
   data(){
     return{
-      titulo : "Alura Fotos"
+      titulo : "Alura Fotos",
+      fotos:[
+        {
+          url : 'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_960_720.jpg',
+          descricao : "Cachorro"
+        },
+        {
+          url : 'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_960_720.jpg',
+          descricao : "Cachorro Grande"
+        }
+      ]
     }
   }
 }
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueResource from 'vue-resource'; //imposrtar do pacote baixado
+//artefatos especializados na realização de requisições assíncronas ao mesmo tempo em que se integra com o ecossistema do Vue.
+Vue.use(VueResource); //usar o pacote
 new Vue({
   el: '#app',
   render: h => h(App)

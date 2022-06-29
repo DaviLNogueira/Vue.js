c2f22faf4d1dd356f95e8a67454ac8de7aad9caf
Davi Lima Nogueira <davi_nogueira@ufms.br>
2022-06-29 10:32:32 -0400

    Aplicação em api e utilização de Lifecycle Hooks

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -18,16 +18,15 @@ export default {
     return{
       titulo : "Alura Fotos",
       fotos:[
-        {
-          url : 'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_960_720.jpg',
-          descricao : "Cachorro"
-        },
-        {
-          url : 'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_960_720.jpg',
-          descricao : "Cachorro Grande"
-        }
+
       ]
     }
+  },
+  //Lifecycle Hooks
+  created() { // usados na inicialização
+    let promisse = this.$http.get('http://localhost:3000/v1/fotos')// usado para capturar a requisão
+      .then(res => res.json())
+      .then(fotos => this.fotos =fotos, error => console.log(error))
   }
 }
 </script>

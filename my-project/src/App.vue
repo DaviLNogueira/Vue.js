08d05b5bfdbe04f6701c903e959d427af1353bee
Davi Lima Nogueira <davi_nogueira@ufms.br>
2022-06-30 09:18:50 -0400

    Aplicação de rotas no menu

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -2,15 +2,35 @@
 
 <template>
   <div class="corpo">
-    <router-view> </router-view>
+    <nav>
+      <ul>
+        <li v-for="route in routes">
+          <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link> <!--Faz com que se estiver na mesma página não recorrega -->
+        </li>
+
+      </ul>
+    </nav>
+    <transition name="pagina">
+      <router-view> </router-view>
+    </transition>
+
   </div>
 
 
 </template>
 
 <script>
+import {routes} from "./routes";
 
 export default {
+  data() {
+
+    return {
+
+      routes
+    }
+
+  }
 
 }
 </script>
@@ -22,5 +42,14 @@ export default {
   width: 96%;
   margin: 0 auto;
 }
+
+.pagina-enter, .pagina-leave-active{ /*São classes que automaticamente são criadas pelo transition tag */
+  opacity: 0;
+}
+
+.pagina-enter-active, .painel-fade-leave-active{
+  transition: opacity .3s; /*Tempo da opacidade*/
+}
+
 
 </style>
--- my-project/src/routes.js
+++ my-project/src/routes.js
@@ -2,7 +2,7 @@ import Cadastro from "./components/cadastro/Cadastro";
 import Home from "./components/home/Home";
 
 export const routes = [
-  {path : '',component : Home},
-  {path: '/cadastro' ,component: Cadastro}
+  {path : '',component : Home, titulo: 'Home'},
+  {path: '/cadastro' ,component: Cadastro, titulo: 'Cadastro'}
 ]; //configurações das rotas de acordo com os componentes
 
